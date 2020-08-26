7453065d5ea1ab647585f88463a2fdc3d5f7a10c
Alex Geoghagan <marisa.geoghagan@revature.net>
2020-08-26 14:41:30 -0400

    Completed tier1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -2,7 +2,34 @@ package com.revature.tier1;
 
 public class NumberSumLength {
 
-	public static boolean checkNumberPowerLength(long num) {
-		return false;
+	public static boolean checkNumberPowerLength(int num) {
+		String stringNum = String.valueOf(num);
+		char[] digits = stringNum.toCharArray();
+		int[] powerDigits = new int[digits.length];
+		int sum = 0;
+		
+		for(int i = 0; i < powerDigits.length; i++) {
+			powerDigits[i] = (int) Math.pow(charToInt(digits[i]), powerDigits.length);
+			sum += powerDigits[i];
+		}
+		
+		return num == sum;
+	}
+
+	public static int charToInt(char input) {
+		boolean isInt = false;
+		
+		try { 
+			isInt = true;
+			Character.getNumericValue(input);
+		}
+		catch(NumberFormatException e) { }
+		catch(NullPointerException e) { }
+		
+		if(isInt) {
+			return Character.getNumericValue(input);
+		} else {
+			return 0;
+		}
 	}
 }
